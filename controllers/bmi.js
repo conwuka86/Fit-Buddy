7532ea8293473452dd4f11890ec08859259b4160
conwuka <ogochuku7@gmail.com>
2025-01-05 20:51:28 -0600

    Add bmi CRUD

---
+++ controllers/bmi.js
@@ -0,0 +1,32 @@
+// routes/bmi.js
+const express = require('express');
+const ensureSignedIn = require('../middleware/ensureSignedIn');
+const User = require('../models/user');
+
+const router = express.Router();
+
+// Get all BMI records
+router.get('/', ensureSignedIn, async (req, res) => {
+  const user = await User.findById(req.user.id);
+  res.render('bmi/index', { bmiData: user.bmiData });
+});
+
+// Add new BMI
+router.post('/', ensureSignedIn, async (req, res) => {
+  const { weight, height } = req.body;
+  const bmi = (weight / ((height / 100) ** 2)).toFixed(2);
+  const user = await User.findById(req.user.id);
+  user.bmiData.push({ weight, height, bmi });
+  await user.save();
+  res.redirect('/bmi');
+});
+
+// Delete BMI record
+router.delete('/:bmiId', ensureSignedIn, async (req, res) => {
+  const user = await User.findById(req.user.id);
+  user.bmiData.id(req.params.bmiId).remove();
+  await user.save();
+  res.redirect('/bmi');
+});
+
+module.exports = router;
