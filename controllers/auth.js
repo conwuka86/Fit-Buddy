13252fc822954a83cdc8f2cc36e83e4359c254bd
conwuka <ogochuku7@gmail.com>
2025-01-05 20:38:34 -0600

    Updae Auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,58 +1,37 @@
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
+// routes/auth.js
 const express = require('express');
-// Create a router function/object (a router is a middleware function)
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
 const router = express.Router();
 
-// All paths start with '/auth'
-
-// GET /auth/sign-up (see the sign-up form - new functionality for the users data resource)
-router.get('/sign-up', (req, res) => {
-  res.render('auth/sign-up.ejs', { title: 'Sign Up!' })
+router.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = new User({ username, password });
+    await user.save();
+    res.redirect('/login');
+  } catch (err) {
+    res.status(400).send('Error creating user');
+  }
 });
 
-// GET /auth/sign-out
-router.get('/sign-out', (req, res) => {
-  req.session.destroy();
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).send('Invalid credentials');
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.cookie('token', token);
+  res.redirect('/dashboard');
+});
+
+router.post('/logout', (req, res) => {
+  res.clearCookie('token');
   res.redirect('/');
 });
-
-// POST /auth/sign-up (create a user)
-router.post('/sign-up', async (req, res) => {
-  try {
-    if (req.body.password !== req.body.confirmPassword) throw new Error('Passwords do not match');
-    req.body.password = bcrypt.hashSync(req.body.password, 6);
-    const user = await User.create(req.body);
-    req.session.user_id = user._id;
-    // Update path to the functionality YOU want
-    res.redirect('/');
-  } catch (e) {
-    console.log(e);
-    res.render('auth/sign-up.ejs', { title: 'Sign Up!' });
-  }
-});
-
-// GET /auth/sign-in (show the sign-in page)
-router.get('/sign-in', (req, res) => {
-  res.render('auth/sign-in.ejs', { title: 'Sign In' });
-});
-
-// POST /auth/sign-in (sign in a user)
-router.post('/sign-in', async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) throw new Error('Invalid username');
-    // Verify the password
-    const valid = bcrypt.compareSync(req.body.password, user.password);
-    if (!valid) throw new Error('Invalid password');
-    req.session.user_id = user._id;
-    // Update path to the functionality YOU want
-    res.redirect('/');
-  } catch (e) {
-    console.log(e);
-    res.render('auth/sign-in.ejs', { title: 'Sign In' });
-  }
-});
-
 
 module.exports = router;
